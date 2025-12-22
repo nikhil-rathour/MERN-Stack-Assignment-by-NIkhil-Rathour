b891b3f54a1cceeff9b1ec6efbce5fdc0e4bb5de
nikhil-rathour <rathournikhil042@gmail.com>
2025-12-22 22:08:57 +0530

    databse config and models  done

---
+++ backend/models/Task.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: { type: String, default: "pending" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+});
+
+module.exports = mongoose.model("Task", taskSchema);
